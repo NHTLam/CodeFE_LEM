c473285cbfbbe3bf735a4fcc357b601a536651c8
NHTLam <105333043+NHTLam@users.noreply.github.com>
2024-04-25 13:46:14 +0700

    Sửa lại một chút giao diện

--- fe_lem_app/app/lem/classroom/[classroomId]/class-work/page.tsx
+++ fe_lem_app/app/lem/classroom/[classroomId]/class-work/page.tsx
@@ -1,5 +1,4 @@
 import { ListClassWork } from "@/components/ComponentsClassroomPage/ListClassWork";
-import { NavClassroom } from "@/components/ComponentsClassroomPage/NavClassroom";
 
 const ClassWork = () => {
   return (
